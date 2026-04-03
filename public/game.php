3f3c76a68b1e10f894825e8229d4df6692827c7d
Ricky <7643361+rbertram90@users.noreply.github.com>
2026-04-03 17:08:12 +0100

    Kill sass

--- public/game.php
+++ public/game.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quizzerino - Game client</title>
-    <link rel="stylesheet" type="text/css" href="/css/dist/game.css">
+    <link rel="stylesheet" type="text/css" href="/css/game.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <!-- <link href="/images/favicon.png" type="image/png" rel="icon"> -->
